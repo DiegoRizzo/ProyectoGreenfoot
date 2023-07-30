b3697407cce8dc970b6ccaa9d2874f4b76d1227c
diegorizzo <diegochunr@hotmail.com>
2023-07-30 13:26:37 -0600

    Mas niveles, cambios al funcionamiento.

---
+++ Proyecto/Proyecto Juego/NUM3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class NUM3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NUM3 extends Actor
+{
+    /**
+     * Act - do whatever the NUM3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
